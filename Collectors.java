29d87b2788174c03a6468003c7d9fb8e842acb6b
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2024-06-19 15:45:12 +0530

    Collectors.toSet()

---
+++ Collectors.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Collectors {
+    
+  
+    public static void main(String args[]) {
+       
+       // Find Duplicates using streams - Assigned to Set to get rid of duplicate values using - toSet()
+      List<String> strings = Arrays.asList("A","B", "A", "C","B","E");
+  
+      System.out.println(Collections.frequency(strings, "A"));
+      Set<String> nList = strings.stream()
+        .filter(s -> Collections.frequency(strings, s) > 1 ).collect(Collectors.toSet());
+       
+      System.out.println(nList);
+        
+    }
+}
